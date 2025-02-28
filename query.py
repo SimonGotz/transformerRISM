9a6264a6a2868a5456efb51d270681ae1fb93745
Simon Gotz <simongotz@msn.com>
2025-02-28 14:19:56 +0100

    Added optimised MAP calculations

--- query.py
+++ query.py
@@ -6,14 +6,10 @@ from random import randint
 import numpy as np
 from statistics import mean
 import sklearn.metrics as metrics
-
-def condensed_index(i, j, m):
-    index = m * i + j - ((i + 2) * (i + 1)) // 2
-    #index = index.long()
-    return index
+import operator
 
 def mean_average_precision(embs, labels, metric):
-    sim_matrix = 1 - metrics.pairwise_distances(embs, metric=metric)
+    sim_matrix = 1 - metrics.pairwise_distances(embs, metric=metric) #dist matrix met euclidean distances
     scores = []
     #labels = np.array(labels)
     for i, sims in enumerate(sim_matrix):
@@ -21,19 +17,17 @@ def mean_average_precision(embs, labels, metric):
         query_y = labels[i]
         target_y = (labels[mask] == query_y).astype(int)
         if target_y.sum() > 0:
+            #score = metrics.average_precision_score(target_y, sims[mask])
             scores.append(metrics.average_precision_score(target_y, sims[mask]))
     return np.mean(scores)
 
-device = torch.device("cuda:0")
 
-pathIncipits = "../Thesis/Data/mtcfsinst2.0_incipits/mtcjson"
+device = torch.device("cuda")
+pathIncipits = "../Thesis/Data/mtcfsinst2.0_incipits(V2)/mtcjson"
 pathWhole = "../Thesis/Data/mtcfsinst2.0/mtcjson"
-features = ["scaledegree","beatfraction","beatstrength"]
-pdist = torch.nn.PairwiseDistance()
-dists = []
+features = ["midipitch","duration","imaweight"]
 meanAP = 0
-transformer = torch.load("../Results/Experiments/{}.pt".format('e45'))
-print(transformer)
+transformer = torch.load("../Weights/HyperparameterTuning/TEST_0.pt", weights_only=False)
 results = []
 precisions = []
 corpus = inputProcessor.Corpus()
@@ -41,32 +35,66 @@ transformer.eval()
 transformer.to(device)
 corpus.readFolder(pathIncipits, features)
 data = corpus.data
+tfsizeDict = {}
 
-tunefamDict = {}
-count = 1
-
-#print(data[0])
-for i in range(len(data)):
-    if i+1 != len(data) and data[i]['tunefamily'] == data[i+1]['tunefamily']:
-        count += 1
-    else:
-        tunefamDict[data[i]['tunefamily']] = count
-        count = 1
+def update_embeddings(transformer, melodies):
+    transformer.eval()
+    start_time = time.time()
     with torch.no_grad():
-        data[i]['Embedding'] = transformer(torch.tensor([data[i]['tokens']]).to(device)).squeeze(0)
+        embs = transformer(melodies.to(device))
+    elapsed = time.time() - start_time
+    print("Embedding calculations: {:5.2f} s".format(elapsed))
+    return embs
 
-labels = [corpus.data[i]['tunefamily'] for i in range(len(corpus.data))]
-labels = np.array(labels)
-embs = [corpus.data[i]['Embedding'] for i in range(len(corpus.data))]
-embs = torch.tensor(torch.stack(embs))
-dm = torch.pdist(embs)
-m = len(data)
-avg_precisions = []
+melodies = []
+labels = []
 
-meanAP = mean_average_precision(embs, labels, 'cosine')
-print(meanAP)
-meanAP = mean_average_precision(embs, labels, 'euclidean')
-print(meanAP)
+for melody in data:
+    melodies.append(melody['tokens'])
+    labels.append(melody['tunefamily'])
+
+for i in range(len(labels)):
+    labels[i] = corpus.tf2label[labels[i]]
+    if labels[i] in tfsizeDict:
+        tfsizeDict[labels[i]] = tfsizeDict[labels[i]] + 1
+    else:
+        tfsizeDict[labels[i]] = 1
+melodies = torch.tensor(melodies)
+embs = update_embeddings(transformer, melodies)
+
+def m_avg_precision(embs, labels, metric):
+    dists = []
+    aps = []
+    dm = metrics.pairwise_distances(embs, metric=metric)
+    for i, dists in enumerate(dm):
+        mask = np.arange(dists.shape[0]) != i
+        query_y = labels[i]
+        y_true = [k for k in labels if labels[k] == query_y and i != k]
+
+        if len(y_true) == 0: # if only 1 melody in tf
+            continue
+
+        indices = sorted(range(len(dists)), key=lambda k: dists[k])
+        indices = [k for k in indices if i != k]
+        count, scores = 0, []
+        for y in y_true:
+            count += 1
+            scores.append(count / (indices.index(y) + 1))
+        aps.append(mean(scores))
+
+    return mean(aps)
+    
+
+#stime = time.time()
+##meanAP = mean_average_precision(embs, labels, 'euclidean')
+#print(f"Karsdorp time:{time.time() - stime}")
+#print(meanAP)
+
+stime = time.time()
+mAP = m_avg_precision(embs, labels, 'euclidean')
+print(f"Own time:{time.time() - stime}")
+print(mAP)
+while True: continue
 
 with open("mAPResults.txt",'r+') as f:
     for i in range(m):
