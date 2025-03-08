3be0b414ae6db2a34590df0dd236804ecea1058c
Simon Gotz <simongotz@msn.com>
2025-03-08 11:10:07 +0100

    Helperfunction for calculating metrics

--- calculateMetrics.py
+++ calculateMetrics.py
@@ -1,3 +1,13 @@
 import query as q
+import inputProcessor as ip
 
-q.calculateMetrics()
+q.calculateMetrics('WholeComplexRandom')
+
+#featuresSimple = ["midipitch","duration","imaweight"]
+#featuresComplex = ["scaledegree","beatfraction","beatstrength"]
+
+#path = "../Thesis/Data/mtcfsinst2.0_incipits(V2)/mtcjson"
+#path = "../Thesis/Data/mtcfsinst2.0/mtcjson"
+
+#corpus = ip.Corpus()
+#corpus.readData(featuresSimple, path, mode='whole')
