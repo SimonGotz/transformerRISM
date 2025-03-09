684221c385ec8bb15e79e4e8a7d66fdf338ab791
Simon Gotz <simongotz@msn.com>
2025-03-09 14:09:29 +0100

    Made colors static

--- clustervisualisation.py
+++ clustervisualisation.py
@@ -7,53 +7,51 @@ from numpy.dtypes import StringDType
 import pandas as pd
 
 tsne = TSNE(n_components=2)
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 
 pathIncipits = "../Thesis/Data/mtcfsinst2.0_incipits(V2)/mtcjson"
 pathWhole = "../Thesis/Data/mtcfsinst2.0/mtcjson"
-features = ["scaledegree","beatfraction","beatstrength"]
+featuresSimple = ["midipitch","duration","imaweight"]
+featuresComplex = ["scaledegree","beatfraction","beatstrength"]
 
-file = "smallTfSetTokenCheck_0"
-transformer = torch.load(f"../Weights/March/Tuning/{file}.pt", weights_only=False)
+file = "WholeComplexRandom"
+transformer = torch.load(f"../Weights/March/Models/{file}.pt", weights_only=False)
 corpus = inputProcessor.Corpus()
 transformer.eval()
 transformer.to(device)
-corpus.readDataSmallModel(features, pathIncipits)
+corpus.readDataSmallModel(featuresComplex, pathWhole, mode='whole')
 
+
+color_discrete_map={"12171_0": 'red',
+    "3680_0": 'dodgerblue',
+    "1212_0": 'black',
+    "7791_0": 'blueviolet',
+    "1703_0": 'brown',
+    "5861_0": 'hotpink',
+    "4882_0": 'cyan',
+    "7116_0": 'lime',
+    "9216_0": 'orange'
+    }
 
 with torch.no_grad():
     embs = transformer(corpus.trainMelodies.to(device))
     labels = corpus.trainLabels
     ids = corpus.trainIds
 
-#for data in corpus.data:
-    #if data['id'] == 'NLB196526_01' or data['id'] == 'NLB177544_01' or data['id'] == 'NLB125141_01':
-        #print(data['tokens'])
-       #print(data['features']['scaledegree'])
-        #print(data['features']['beatstrength'])
-        #print(data['features']['beatfraction'])
-        #print(data['id'])
-
 def showCV(embs, labels, ids):
     labels = np.array(labels, dtype=StringDType)
     X = tsne.fit_transform(embs)
     dataDict = {"embs1": X[:, 0],
                 "embs2": X[:, 1],
-
                 "labels": labels,
                 "ids": ids
                 }
     df = pd.DataFrame(dataDict)
-    df.sort_values(by=['labels'])
-    outlier = df[df['labels'] == '4882_0']
-    print(outlier)
 
-    while True: continue
-
-    fig = px.scatter(df, x='embs1', y='embs2', color='labels', hover_data={'ids':True})
+    fig = px.scatter(df, x='embs1', y='embs2', color='labels', color_discrete_map=color_discrete_map, hover_data={'ids':True})
     fig.update_traces(marker={'size': 20})
     fig.update_layout(
-        title="2D Clustering on small dataset using T-sne",
+        title="2D Clustering on small dataset using model 7",
         xaxis_title="First t-SNE",
         yaxis_title="Second t-SNE",
         font=dict(
